f3c7403790cf0a38ec43960d17be1046249055b2
DeathG7n <christariccykid55@gmail.com>
2022-07-15 14:24:34 +0100

    fixed login error

--- src/components/api/axiosConfig.js
+++ src/components/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const API = axios.create({
     // baseURL: "https://duduzili.com/api/v1/rest-auth/",
-  baseURL: "http://134.122.31.135/api/v1/rest-auth/",
+  baseURL: "https://134.122.31.135/api/v1/rest-auth/",
   //  baseURL:
   //        "http://api.weatherapi.com/v1/current.json?key=14638040845c4a4f96b101259222406 &q=Nigeria&aqi=yes",
   headers: {
